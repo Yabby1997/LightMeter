ccd573037ad5ee0de383aa5f5e8b464f75ae6eda
Seunghun Yang <yabby1997@gmail.com>
2023-11-17 20:30:06 +0900

    Add documentations

--- LightMeter/Sources/LightMeterService.swift
+++ LightMeter/Sources/LightMeterService.swift
@@ -8,13 +8,24 @@
 
 import Foundation
 
+/// A service for light meter functionality, providing exposure value calculations.
 public class LightMeterService {
+
+    /// Initializes a new instance of LightMeterService.
     public init() {}
-    
+
     public func foo() -> String {
         "Hello from LightMeter"
     }
-    
+
+    /// Calculates the Exposure Value (EV) based on ISO, shutter speed, and aperture.
+    ///
+    /// - Parameters:
+    ///   - iso: The sensitivity value for EV calculation.
+    ///   - shutterSpeed: The shutter speed value for EV calculation.
+    ///   - aperture: The aperture value for EV calculation.
+    ///
+    /// - Returns: The calculated Exposure Value as an integer.
     public func getExposureValue(iso: Int, shutterSpeed: Float, aperture: Float) -> Int {
         Int(round(log2((Float(100) * aperture * aperture) / (shutterSpeed * Float(iso)))))
     }
